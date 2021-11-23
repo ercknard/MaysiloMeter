69598471272a93052e78061541de08de827f806a
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-24 05:31:28 +0800

    Update sms.php

--- Regusers/sms.php
+++ Regusers/sms.php
@@ -70,6 +70,7 @@ function sendSMS($cnumber, $vcode){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMS Verification</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; }
