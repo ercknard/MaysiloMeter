a0fc7988e9b63a60325708d162a9da41a4cea962
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-24 04:50:35 +0800

    new

---
+++ Regusers/check.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "b64914f07d5e65";
+$password = "f742c533";
+$dbname = "heroku_5142987c57081aa";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$inputtedCode = $_POST['code'];
+
+$correctCode = $_SESSION['otp'];
+$username = $_SESSION['username'];
+$contact = $_SESSION['contact'];
+
+if($inputtedCode == $correctCode){
+    $sql = "INSERT INTO tblusers (username, contact)
+    VALUES ('".$username."','".$contact."')";
+    
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['message_type'] = 'success';
+        $_SESSION['message'] = 'User has been inserted successfully!';
+    } else {
+        $_SESSION['message_type'] = 'warning';
+        $_SESSION['message'] = 'Something is wrong!';
+    }
+
+    header('location: reg.php');
+}else{
+    $_SESSION['message_type'] = 'danger';
+    $_SESSION['message'] = 'Incorrect Code. Please try again!';
+    header('location: reg.php');
+}
+?>
---
+++ Regusers/database.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "b64914f07d5e65";
+$password = "f742c533";
+$dbname = "heroku_5142987c57081aa";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "ALTER TABLE tblusers
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6";
+
+if($conn->query($sql) === TRUE) {
+  echo "Table successfully updated";
+} else {
+  echo "Error updating table: " . $conn->error;
+}
+?>
