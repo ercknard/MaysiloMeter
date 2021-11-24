b4751cadcbb14aec9bf8bd32be2b4634f2bb541a
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-25 02:39:39 +0800

    Update Dashboardini.php

--- Admin/AdminDashboard/Dashboardini.php
+++ Admin/AdminDashboard/Dashboardini.php
@@ -40,7 +40,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">User Details       </h2>
-                        <a href="create.php" class="btn btn-primary pull-right"><i class="fa fa-plus"></i> Add New User</a><p>  </p><a href="https://maysilometer.herokuapp.com/Admin/logout.php" class="btn btn-danger pull-right">Sign Out</a>
+                        <a href="create.php" class="btn btn-primary pull-right mr-1"><i class="fa fa-plus"></i> Add New User</a><p>  </p><a href="https://maysilometer.herokuapp.com/Admin/logout.php" class="btn btn-danger pull-right">Sign Out</a>
                     </div>
                     <?php
                     // Include config file
