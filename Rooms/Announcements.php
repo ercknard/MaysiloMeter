a55b9989db3500f200c01b09a8dcd49639d094e2
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-25 11:58:23 +0800

    Update Announcements.php

--- Rooms/Announcements.php
+++ Rooms/Announcements.php
@@ -5,14 +5,14 @@
     <meta charset="utf-8">
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <meta http-equiv="X-Frame-Options" content="SAMEORIGIN">
     <title>Announcements</title>
     <link rel="stylesheet" href="../nicepage.css" media="screen">
 <link rel="stylesheet" href="Announcements.css" media="screen">
     <script class="u-script" type="text/javascript" src="../jquery.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="../nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 3.29.1, nicepage.com">
+    <meta name="generator" content="Nicepage 4.0.3, nicepage.com">
     <link rel="icon" href="images/favicon.png">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
     <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Titillium+Web:200,200i,300,300i,400,400i,600,600i,700,700i,900">
@@ -34,8 +34,8 @@
     <meta property="og:description" content="">
     <meta property="og:type" content="website">
   </head>
-  <body class="u-body"><header class="u-clearfix u-grey-10 u-header u-sticky u-header" id="sec-5caa"><div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-xl u-sheet-1">
-        <a href="../Home.html" data-page-id="63320719" class="u-image u-logo u-image-1" data-image-width="309" data-image-height="104" title="home">
+  <body class="u-body"><header class="u-clearfix u-grey-10 u-header u-sticky u-header" id="sec-5caa"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="/Home.html" data-page-id="63320719" class="u-image u-logo u-image-1" data-image-width="309" data-image-height="104" title="home">
           <img src="../images/banner.png" class="u-logo-image u-logo-image-1">
         </a>
         <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
@@ -48,25 +48,25 @@
             </a>
           </div>
           <div class="u-custom-menu u-nav-container">
-            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="../Home.html" style="padding: 10px 20px;">Home</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Announcements.html" style="padding: 10px 20px;">Announcements</a>
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/Rooms/About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/Rooms/Announcements.html" style="padding: 10px 20px;">Announcements</a>
 </li></ul>
           </div>
           <div class="u-custom-menu u-nav-container-collapse">
             <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
               <div class="u-inner-container-layout u-sidenav-overflow">
                 <div class="u-menu-close"></div>
-                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="../Home.html" style="padding: 10px 20px;">Home</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html" style="padding: 10px 20px;">About</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Announcements.html" style="padding: 10px 20px;">Announcements</a>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/Rooms/About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/Rooms/Announcements.html" style="padding: 10px 20px;">Announcements</a>
 </li></ul>
               </div>
             </div>
             <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
           </div>
         </nav>
-        <a href="../Registration/Register.html" data-page-id="130709230" class="u-btn u-btn-round u-button-style u-gradient u-none u-radius-4 u-text-body-alt-color u-btn-1">Register</a>
+        <a href="/Registration/Register.html" data-page-id="130709230" class="u-btn u-btn-round u-button-style u-gradient u-none u-radius-4 u-text-body-alt-color u-btn-1">Register</a>
       </div></header>
     <section class="u-clearfix u-section-1" id="sec-8653">
       <div class="u-clearfix u-sheet u-sheet-1">
@@ -77,7 +77,7 @@
           <style></style>
           <iframe width="400px" height="400px" frameborder="0" src=" https://industrial.ubidots.com/app/dashboards/public/widget/oUzK5vzwOCsym1qMEzaHHE7bE64Yz7LtocVlxDlu1Vw?embed=true"></iframe>
         </div>
-        <div class="u-border-1 u-border-grey-75 u-clearfix u-custom-html u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-gradient u-custom-html-2">
+        <div class="u-border-1 u-border-grey-75 u-clearfix u-custom-html u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-gradient u-custom-html-2">
           <style></style>
           <iframe width="100%" height="400px" frameborder="0" src=" https://industrial.ubidots.com/app/dashboards/public/widget/oezD4U97mHTRvGCkL1ptknpAjIbkrjZbrLK5c5hRink?embed=true"></iframe>
         </div>
@@ -86,16 +86,16 @@
       </div>
     </section>
     <section class="u-clearfix u-gradient u-section-2" id="sec-f241">
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+      <div class="u-clearfix u-sheet u-sheet-1">
         <h2 class="u-custom-font u-font-titillium-web u-text u-text-white u-text-1">
           <span style="font-size: 1.875rem;">Announcements :</span>
         </h2><!--blog--><!--blog_options_json--><!--{"type":"Recent","source":"","tags":"","count":""}--><!--/blog_options_json-->
-        <div class="u-blog u-blog-1">
+        <div class="u-blog u-expanded-width u-blog-1">
           <div class="u-repeater u-repeater-1"><!--blog_post-->
             <div class="u-blog-post u-container-style u-repeater-item u-white u-repeater-item-1">
               <div class="u-container-layout u-similar-container u-container-layout-1">
                 <a class="u-post-header-link" href="blog/post-3.html"><!--blog_post_image-->
-                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-1" src="../images/Untitledbanner.png"><!--/blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-1" data-image-width="500" data-image-height="500" src="../images/floodlogo.png"><!--/blog_post_image-->
                 </a><!--blog_post_header-->
                 <h4 class="u-blog-control u-text u-text-2">
                   <a class="u-post-header-link" href="blog/post-3.html"><!--blog_post_header_content-->Post 3<!--/blog_post_header_content--></a>
@@ -107,7 +107,7 @@
             <div class="u-blog-post u-container-style u-repeater-item u-video-cover u-white">
               <div class="u-container-layout u-similar-container u-container-layout-2">
                 <a class="u-post-header-link" href="blog/brgy-maysilo-post.html"><!--blog_post_image-->
-                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-2" src="../images/Untitledbanner.png"><!--/blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-2" src="../images/floodlogo.png" data-image-width="500" data-image-height="500"><!--/blog_post_image-->
                 </a><!--blog_post_header-->
                 <h4 class="u-blog-control u-text u-text-4">
                   <a class="u-post-header-link" href="blog/brgy-maysilo-post.html"><!--blog_post_header_content-->Brgy Maysilo Post<!--/blog_post_header_content--></a>
@@ -119,7 +119,7 @@
             <div class="u-blog-post u-container-style u-repeater-item u-video-cover u-white">
               <div class="u-container-layout u-similar-container u-container-layout-3">
                 <a class="u-post-header-link" href="blog/announcement-sample-1.html"><!--blog_post_image-->
-                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-3" src="../images/Untitledbanner.png"><!--/blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-3" src="../images/floodlogo1.png" data-image-width="500" data-image-height="500"><!--/blog_post_image-->
                 </a><!--blog_post_header-->
                 <h4 class="u-blog-control u-text u-text-6">
                   <a class="u-post-header-link" href="blog/announcement-sample-1.html"><!--blog_post_header_content-->Announcement sample 1<!--/blog_post_header_content--></a>
@@ -136,11 +136,10 @@
     <section class="u-clearfix u-gradient u-hidden-xs u-section-3" id="sec-c223">
       <div class="u-clearfix u-sheet u-sheet-1">
         <div class="u-clearfix u-custom-html u-expanded-width u-custom-html-1">
-          <iframe src="http://121.58.193.173:8080/" style="width: 100%; 
-            height:1000px"> Sorry your browser does not support inline frames. </iframe>
+          <embed src="https://zoom.earth/#view=14.679,120.962,7z/date=2021-11-24,11:10,+8/layers=base,wind,fires,crosshairs" width="100%" height="1000px" onerror="alert('URL invalid !!');">
         </div>
-        <h2 class="u-align-center-md u-align-center-sm u-align-center-xs u-custom-font u-font-titillium-web u-text u-text-white u-text-1">Interactive Satellite viewer by PAG-ASA : Philippine Atmospheric, Geophysical and Astronomical Services Administration. </h2>
-        <h2 class="u-align-center-md u-align-center-sm u-align-center-xs u-custom-font u-font-titillium-web u-text u-text-white u-text-2" data-animation-name="flash" data-animation-duration="1000" data-animation-delay="0" data-animation-direction="">Note: If the interactive viewer does not load, kindly click the button below and It will redirect you to PAG-ASA viewer.</h2>
+        <h2 class="u-align-center-sm u-align-center-xs u-custom-font u-font-titillium-web u-text u-text-white u-text-1">Interactive Satellite viewer of wind focusing Barangay of Maysilo by Zoom Earth&nbsp;</h2>
+        <h2 class="u-align-center-md u-align-center-sm u-align-center-xs u-custom-font u-font-titillium-web u-text u-text-white u-text-2" data-animation-name="flash" data-animation-duration="1000" data-animation-delay="0" data-animation-direction="">Note: If you want to view the weather forcast, kindly click the button below and It will redirect you to PAG-ASA viewer.</h2>
         <a href="http://121.58.193.173:8080/" class="u-btn u-btn-round u-button-style u-gradient u-none u-radius-4 u-text-body-alt-color u-btn-1" target="_blank">View</a>
       </div>
     </section>
@@ -155,32 +154,3 @@
       </div></footer>
   </body>
 </html>
-<?php
-// Allow from any origin
-if(isset($_SERVER["HTTP_ORIGIN"]))
-{
-    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-}
-else
-{
-    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
-    header("Access-Control-Allow-Origin: *");
-}
-
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 600");    // cache for 10 minutes
-
-if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
-{
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
-        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
-
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    //Just exit with 200 OK with the above headers for OPTIONS method
-    exit(0);
-}
-//From here, handle the request as it is ok
-?>
