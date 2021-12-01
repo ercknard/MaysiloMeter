ff908b58749d20ecca6e147d16673f79522a7697
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-01 12:27:34 +0800

    12

--- Regusers/sms.php
+++ Regusers/sms.php
@@ -51,16 +51,7 @@ function sendSMS($cnumber, $vcode){
         ),
         ));
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
 
-        curl_close($curl);
-
-        if ($err) {
-        echo "cURL Error #:" . $err;
-        } else {
-        echo $response;
-        }
     }
 ?>
 
