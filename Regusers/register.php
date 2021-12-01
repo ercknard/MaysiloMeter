ac1a8e03bc10cae28cc4e11e39e659e6cda7e645
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-01 12:33:35 +0800

    12

--- Regusers/register.php
+++ Regusers/register.php
@@ -22,11 +22,6 @@ session_start();
 <hr color="lightblue" width="100%">
         <h2>User Registration</h2>
         <p>If you wish to have flood alerts through sms kindly fill out the form.</p>
-        <?php if(isset($_SESSION['message'])): ?>
-            <div class="alert alert-<?= $_SESSION['message_type'] ?>" role="alert">
-                <?= $_SESSION['message'] ?>
-            </div>
-        <?php endif ?>  
             <form method="POST" action="sms.php" width="100%">
             <div class="form-group">
                 <label>Username</label>
