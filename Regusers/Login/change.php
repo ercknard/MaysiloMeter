5948a9ad36f305cd06146bd6fb09f8a43e8da1e3
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-02 20:29:52 +0800

    Update change.php

--- Regusers/Login/change.php
+++ Regusers/Login/change.php
@@ -60,6 +60,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+        
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $username = $row["username"];
+                    $contact = $row["contact"];
+                    $created_at = $row["created_at"];
+                } else{
+                    // URL doesn't contain valid id parameter. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            }
+
             // Close statement
             mysqli_stmt_close($stmt);
         }
@@ -70,54 +90,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
  
- <?php
-// Check existence of id parameter before processing further
-if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-    
-    // Prepare a select statement
-    $sql = "SELECT * FROM tblusers WHERE id = ?";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
-        // Set parameters
-        $param_id = trim($_GET["id"]);
-        
-        // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
-            $result = mysqli_stmt_get_result($stmt);
-    
-            if(mysqli_num_rows($result) == 1){
-                /* Fetch result row as an associative array. Since the result set
-                contains only one row, we don't need to use while loop */
-                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                
-                // Retrieve individual field value
-                $username = $row["username"];
-                $contact = $row["contact"];
-                $created_at = $row["created_at"];
-            } else{
-                // URL doesn't contain valid id parameter. Redirect to error page
-                header("location: error.php");
-                exit();
-            }
-            
-        } else{
-            echo "Oops! Something went wrong. Please try again later.";
-        }
-    }
-     
-    // Close statement
-    mysqli_stmt_close($stmt);
-    
-    // Close connection
-    mysqli_close($link);
-} else{
-    // URL doesn't contain id parameter. Redirect to error page
-    header("location: error.php");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
