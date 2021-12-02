331d8dfdb326a2404781d608954fa94b0c76aafd
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-02 18:11:50 +0800

    Update sample.php

--- Regusers/sample.php
+++ Regusers/sample.php
@@ -91,7 +91,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: change.php");
+                header("location: Login/change.php");
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
