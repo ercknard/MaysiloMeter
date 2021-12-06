e02276fef95d8d44b58e046c8040de1be20660fc
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 17:14:55 +0800

    12

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -1,66 +1,34 @@
+<?php  
 
-<?php
 
 //open connection to mysql db
 $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($con));
 
-$sql = "select * from tblusers";
-$result = mysqli_query($con, $sql) or die("Error in Selecting " . mysqli_error($con));
-$row_count = mysqli_num_rows($result);
-// free the result set as you don't need it anymore
-mysqli_free_result($result);
+    $limit = 3;  //set  Number of entries to show in a page.
+    // Look for a GET variable page if not found default is 1.        
+    if (isset($_GET["page"])) {    
+    $page  = $_GET["page"];    
+    }    
+    else { $page=1;    
+    } 
+    //determine the sql LIMIT starting number for the results on the displaying page  
+    $page_index = ($page-1) * $limit;      // 0
 
-$page_count = 0;
-if (0 === $row_count) {  
-    // maybe show some error since there is nothing in your table
-} else {
-   // determine page_count
-   $page_count = (int)ceil($row_count / $items_per_page);
-   // double check that request page is in range
-   if($page > $page_count) {
-        // error to user, maybe set page to 1
-        $page = 1;
-   }
-}
-
-// determine page number from $_GET
-$page = 1;
-if(!empty($_GET['page'])) {
-    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-    if(false === $page) {
-        $page = 1;
+    $All_Users=mysqli_query($con,"select * from tblusers limit $page_index, $limit");
+    while($row=mysqli_fetch_array($All_Users))
+    {
+        //show  data in table or where you want..
     }
-}
-
-// set the number of items to display per page
-$items_per_page = 10;
-
-// build query
-$offset = ($page - 1) * $items_per_page;
-$sql = "select * from tblusers LIMIT " . $offset . "," . $items_per_page;
-
-// make your LIMIT query here as shown above
-
-
-// later when outputting page, you can simply work with $page and $page_count to output links
-// for example
-for ($i = 1; $i <= $page_count; $i++) {
-    if ($i === $page) { // this is current page
-        echo 'Page ' . $i . '<br>';
-    } else { // show link to other page   
-        echo '<a href="/dex.php?page=' . $i . '">Page ' . $i . '</a><br>';
-    }
- }
-
-//create an array11
-$mirmo = array();
-while($row =mysqli_fetch_assoc($result))
-{
-    $mirmo[] = $row; 
-}
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($mirmo);
-
-//close the db connections
-mysqli_close($con);
+    $all_data=mysqli_query($con,"select count(*) from tblusers");
+    $user_count = mysqli_fetch_row($all_data);   // say total count 9  
+    $total_records = $user_count[0];   //9
+    $total_pages = ceil($total_records / $limit);    // 9/3=  3
+    if($page >= 2){
+        echo "<a href='blog.php?page=".($page-1)."' class='btn 
+     customBtn2'>Previous</a>";
+      }
+    
+    if($page<$total_pages) {
+        echo "<a href='blog.php?page=".($page+1)."' class='btn customBtn2'>NEXT</a>";   
+    }       
 ?>
