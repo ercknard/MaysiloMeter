b04bded467cc93dbd248aa1aa522c8886e1bfc34
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 18:34:10 +0800

    Update index.php

--- PageCount/index.php
+++ PageCount/index.php
@@ -4,7 +4,7 @@
 //open connection to mysql db
 $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($con));
 
-$sql = "select count(id) AS NumberOFdata from tblusers";
+$sql = "select count(*) AS NumberOFdata from tblusers";
 $result = mysqli_query($con, $sql) or die("Error in Selecting " . mysqli_error($con));
 
 $user_count = mysqli_fetch_row($result);   // say total count 9  
