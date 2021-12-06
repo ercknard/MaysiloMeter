01ae4e81fd91fa3d8e9fb5d283875433290514ed
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 17:44:00 +0800

    Update dex.php

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -19,7 +19,7 @@ $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533",
     {
         $mirmo[] = $row;
     }
-    $all_data=mysqli_query($con,$All_Users);
+    $all_data=mysqli_query($con,"select * from tblusers");
     $user_count = mysqli_fetch_row($all_data);   // say total count 9  
     $total_records = $user_count[0];   //9
     $total_pages = ceil($total_records / $limit);    // 9/3=  3
