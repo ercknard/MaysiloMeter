e2d4acb28032c4882b068c91f1c13878174390b2
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 16:41:24 +0800

    12

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -2,25 +2,60 @@
 <?php
 
 //open connection to mysql db
-$connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
+$con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
-$page_number = mysqli_escape_string($con, $_GET['page']);
+$sql = "SELECT * FROM tblusers";
+$result = mysqli_query($con, $sql);
+$row_count = mysqli_num_rows($result);
+// free the result set as you don't need it anymore
+mysqli_free_result($result);
+
+$page_count = 0;
+if (0 === $row_count) {  
+    // maybe show some error since there is nothing in your table
+} else {
+   // determine page_count
+   $page_count = (int)ceil($row_count / $items_per_page);
+   // double check that request page is in range
+   if($page > $page_count) {
+        // error to user, maybe set page to 1
+        $page = 1;
+   }
+}
+
+// determine page number from $_GET
+$page = 1;
+if(!empty($_GET['page'])) {
+    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+    if(false === $page) {
+        $page = 1;
+    }
+}
+
+// set the number of items to display per page
+$items_per_page = 10;
+
+// build query
+$offset = ($page - 1) * $items_per_page;
+$sql = "SELECT * FROM tblusers LIMIT " . $offset . "," . $items_per_page;
+
+// make your LIMIT query here as shown above
+
+
+// later when outputting page, you can simply work with $page and $page_count to output links
+// for example
+for ($i = 1; $i <= $page_count; $i++) {
+    if ($i === $page) { // this is current page
+        echo 'Page ' . $i . '<br>';
+    } else { // show link to other page   
+        echo '<a href="/dex.php?page=' . $i . '">Page ' . $i . '</a><br>';
+    }
+ }
+$page_number = mysqli_escape_string($connection, $_GET['page']);
 $count_per_page = 10;
 $next_offset = $page_number * $count_per_page;
 
-//fetch table rows from mysql db
-$sql = "select * from tblusers LIMIT $count_per_page OFFSET $next_offset";
-$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
-
-//create an array11
-$mirmo = array();
-while($row =mysqli_fetch_assoc($result))
-{
-    $mirmo[] = $row; 
-}
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($mirmo);
 
 //close the db connections
-mysqli_close($connection);
+mysqli_close($con);
 ?>
