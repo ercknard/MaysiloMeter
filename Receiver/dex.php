1c87e0df96362f722da744284e66e926ad54f4fc
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 17:24:04 +0800

    12

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -23,6 +23,10 @@ $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533",
     $user_count = mysqli_fetch_row($all_data);   // say total count 9  
     $total_records = $user_count[0];   //9
     $total_pages = ceil($total_records / $limit);    // 9/3=  3
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($all_data);
+    
     if($page >= 2){
         echo "<a href='dex.php?page=".($page-1)."' class='btn 
      customBtn2'>Previous</a>";
