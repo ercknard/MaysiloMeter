7a9a5ab61461d2fc7aca1ace3a7762736990dad4
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 17:51:38 +0800

    12

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -19,10 +19,6 @@ $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533",
     {
         $mirmo[] = $row;
     }
-    $all_data=mysqli_query($con,"select count(*) from tblusers");
-    $user_count = mysqli_fetch_row($all_data);   // say total count 9  
-    $total_records = $user_count[0];   //9
-    $total_pages = ceil($total_records / $limit);    // 9/3=  3
 
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($mirmo);   
