6123584847be47d47e12c903ba48bfb429a11c1d
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 16:30:33 +0800

    Update dex.php

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -4,8 +4,12 @@
 //open connection to mysql db
 $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
+$page_number = mysqli_escape_string($con, $_GET['page']);
+$count_per_page = 10;
+$next_offset = $page_number * $count_per_page;
+
 //fetch table rows from mysql db
-$sql = "select * from tblusers LIMIT 10 OFFSET 0";
+$sql = "select * from tblusers LIMIT $count_per_page OFFSET $next_offset";
 $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
 //create an array11
