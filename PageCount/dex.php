60d50b0cf3d4bd8a530fe05d4f11f295b2f1a58c
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 20:37:40 +0800

    Update dex.php

--- PageCount/dex.php
+++ PageCount/dex.php
@@ -6,11 +6,13 @@ $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533",
 
 $limit = 10;
 $result=mysqli_query($con,"SELECT COUNT(*) as total from tblusers");
-$user_count = mysqli_fetch_row($result);   // say total count 9  
-$total_records = $user_count[0];   //9
+$user_count = mysqli_fetch_row($result);  
+$total_records = $user_count[0];
 $total_pages = ceil($total_records / $limit);
 
 $mirmo = array($total_pages);
+while($row=mysqli_fetch_assoc($mirmo))
+
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($mirmo);   // 9/3=  3
