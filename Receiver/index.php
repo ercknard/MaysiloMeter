42a3a18d94d0e678658655be955c6a0c5d671fac
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 10:25:56 +0800

    12

--- Receiver/index.php
+++ Receiver/index.php
@@ -1,4 +1,24 @@
 <?php
+header("Content-Type: application/json");
+
+// Collect what you need in the $data variable.
+
+$json = json_encode($data);
+if ($json === false) {
+    // Avoid echo of empty string (which is invalid JSON), and
+    // JSONify the error message instead:
+    $json = json_encode(["jsonError" => json_last_error_msg()]);
+    if ($json === false) {
+        // This should not happen, but we go all the way now:
+        $json = '{"jsonError":"unknown"}';
+    }
+    // Set HTTP response status code to: 500 - Internal Server Error
+    http_response_code(500);
+}
+echo $json;
+?>
+<?php
+
     //open connection to mysql db
     $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
