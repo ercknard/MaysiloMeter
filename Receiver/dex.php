f691cf87b5c6897d375066b0a15b783f72803d4d
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 16:16:45 +0800

    12

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -5,8 +5,6 @@
 $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
 //fetch table rows from mysql db
-$sql = "select * from tblusers";
-$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
 $page_number = mysqli_escape_string($con, $_GET['page']);
 $count_per_page = 10;
