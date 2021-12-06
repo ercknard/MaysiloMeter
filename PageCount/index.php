63d05ca976b1195b431a12dd869f973097cf96b0
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 18:50:41 +0800

    12

--- PageCount/index.php
+++ PageCount/index.php
@@ -4,16 +4,12 @@
 //open connection to mysql db
 $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($con));
 
-$sql = "select count(*) AS NumberOFdata from tblusers";
-$result=mysqli_query($con,"SELECT COUNT(*) as total from tblusers");
-$data=mysqli_fetch_assoc($result);
-echo $data['total'];
-
 $limit = 10;
+$result=mysqli_query($con,"SELECT COUNT(*) as total from tblusers");
 $user_count = mysqli_fetch_row($result);   // say total count 9  
 $total_records = $user_count[0];   //9
 $total_pages = ceil($total_records / $limit);
-echo $total_pages['total'];    // 9/3=  3
+echo $total_pages;   // 9/3=  3
 
 
     //close the db connections
