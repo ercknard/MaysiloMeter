592750510cc3a5a36f55c74fa768e8d2a1682dac
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 16:22:37 +0800

    Update dex.php

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -2,19 +2,20 @@
 <?php
 
 //open connection to mysql db
-$con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
+$connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
 //fetch table rows from mysql db
+$sql = "select * from tblusers LIMIT 10 OFFSET 0";
+$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
-$page_number = mysqli_escape_string($con, $_GET['page']);
-$count_per_page = 10;
-$next_offset = $page_number * $count_per_page;
-$cat =mysqli_query($con, "SELECT * FROM tblusers LIMIT $count_per_page OFFSET $next_offset");
-while ($row = mysqli_fetch_array($cat))
-        $count = $row[0];
-
+//create an array11
+$mirmo = array();
+while($row =mysqli_fetch_assoc($result))
+{
+    $mirmo[] = $row; 
+}
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode($cat);
+echo json_encode($mirmo);
 
 //close the db connections
 mysqli_close($connection);
