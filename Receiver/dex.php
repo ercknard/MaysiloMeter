eae88f611d2930ac948f1b5bc3a8488c703eb84f
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 17:19:38 +0800

    Update dex.php

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -4,7 +4,7 @@
 //open connection to mysql db
 $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($con));
 
-    $limit = 3;  //set  Number of entries to show in a page.
+    $limit = 10;  //set  Number of entries to show in a page.
     // Look for a GET variable page if not found default is 1.        
     if (isset($_GET["page"])) {    
     $page  = $_GET["page"];    
@@ -19,7 +19,7 @@ $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533",
     {
         //show  data in table or where you want..
     }
-    $all_data=mysqli_query($con,"select count(*) from tblusers");
+    $all_data=mysqli_query($con,"select * from tblusers");
     $user_count = mysqli_fetch_row($all_data);   // say total count 9  
     $total_records = $user_count[0];   //9
     $total_pages = ceil($total_records / $limit);    // 9/3=  3
