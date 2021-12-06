2ce44e161149416d060034613c72413dd1d822b3
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 12:36:24 +0800

    12

--- Receiver/index.php
+++ Receiver/index.php
@@ -20,29 +20,3 @@
     //close the db connections
     mysqli_close($connection);
 ?>
-<?php
-
-if(isset($_SERVER["HTTP_ORIGIN"]))
-{
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-}
-else
-{
-    header("Access-Control-Allow-Origin: *");
-}
-
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 600");
-
-if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
-{
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
-        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
-
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-}
-
-?>
