b967cda5606e1e7e03655662122a433f180d1d4c
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-06 16:15:25 +0800

    12

--- Receiver/dex.php
+++ Receiver/dex.php
@@ -8,47 +8,16 @@ $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f74
 $sql = "select * from tblusers";
 $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
-$row_count = mysqli_num_rows($result);
-// free the result set as you don't need it anymore
-mysqli_free_result($result);
+$page_number = mysqli_escape_string($con, $_GET['page']);
+$count_per_page = 10;
+$next_offset = $page_number * $count_per_page;
+$cat =mysqli_query($con, "SELECT * FROM tblusers LIMIT $count_per_page OFFSET $next_offset");
+while ($row = mysqli_fetch_array($cat))
+        $count = $row[0];
 
-$page_count = 0;
-if (0 === $row_count) {  
-    // maybe show some error since there is nothing in your table
-} else {
-   // determine page_count
-   $page_count = (int)ceil($row_count / $items_per_page);
-   // double check that request page is in range
-   if($page > $page_count) {
-        // error to user, maybe set page to 1
-        $page = 1;
-   }
-}
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($cat);
 
-// determine page number from $_GET
-$page = 1;
-if(!empty($_GET['page'])) {
-    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-    if(false === $page) {
-        $page = 1;
-    }
-}
-
-// set the number of items to display per page
-$items_per_page = 10;
-
-// build query
-$offset = ($page - 1) * $items_per_page;
-$sql = "SELECT * FROM tblusers LIMIT " . $offset . "," . $items_per_page;
-
-
-// later when outputting page, you can simply work with $page and $page_count to output links
-// for example
-for ($i = 1; $i <= $page_count; $i++) {
-   if ($i === $page) { // this is current page
-       echo 'Page ' . $i . '<br>';
-   } else { // show link to other page   
-       echo '<a href="/index.php?page=' . $i . '">Page ' . $i . '</a><br>';
-   }
-} 
+//close the db connections
+mysqli_close($connection);
 ?>
