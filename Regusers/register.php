a0f8c657818a8f34aca01415fab25ba5d59ef6d0
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-08 00:53:36 +0800

    12

--- Regusers/register.php
+++ Regusers/register.php
@@ -123,7 +123,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   oninput="this.setCustomValidity('')" minlength="6">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
-
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" id="confirm_password" placeholder="Repeat password." class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>" required oninvalid="this.setCustomValidity('Verify your password here.')"
+  oninput="this.setCustomValidity('')" minlength="6">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
                 <input value="Register" type="submit" class="btn btn-primary btn-block">
 
                 <hr color="lightblue" width="100%">
