2ca57a460aa5ed31b5bb57de6334458a161e10d7
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-08 00:48:24 +0800

    12

--- Regusers/register.php
+++ Regusers/register.php
@@ -83,7 +83,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <script src="new.js"></script>
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 400px; padding: 20px; margin-left: auto; margin-right: auto;}
