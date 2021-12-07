f754f5c87cb1b7c8a95a55f831f1c3c3ed7e4f02
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-08 00:27:04 +0800

    12

--- Regusers/new.js
+++ Regusers/new.js
@@ -1,11 +1,10 @@
-
-var check = function() {
- if (document.getElementById('password').value ==
-     document.getElementById('confirm_password').value) {
-     document.getElementById('message').style.color = 'green';
-     document.getElementById('message').innerHTML = 'matching';
- } else {
-         document.getElementById('message').style.color = 'red';
-     document.getElementById('message').innerHTML = 'not matching';
- }
-}
+function matchPassword() {  
+    var pw1 = document.getElementById("password");  
+    var pw2 = document.getElementById("confirm_password");  
+    if(pw1 != pw2)  
+    {   
+      alert("Passwords did not match");  
+    } else {  
+      alert("Password created successfully");  
+    }  
+  }  
