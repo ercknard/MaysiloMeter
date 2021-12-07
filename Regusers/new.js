018ce9b712d6944df2929d8b13d11a191dcda048
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-08 01:09:24 +0800

    32

--- Regusers/new.js
+++ Regusers/new.js
@@ -1,5 +1,5 @@
-var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
+var password = document.getElementById("password1")
+  , confirm_password = document.getElementById("confirm_password1");
 
 function validatePassword(){
   if(password.value != confirm_password.value) {
