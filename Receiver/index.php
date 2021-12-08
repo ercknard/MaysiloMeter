013c95106e870d16e9ca4a6d11b46ab16f3d705d
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-08 10:41:13 +0800

    Update index.php

--- Receiver/index.php
+++ Receiver/index.php
@@ -16,20 +16,26 @@ $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533",
     //determine the sql LIMIT starting number for the results on the displaying page  
     $page_index = ($page-1) * $limit;      // 0
 
-    $All_Users=mysqli_query($con,"select contacts from tblusers limit $page_index, $limit");
+    $result=mysqli_query($con,"select contact from tblusers limit $page_index, $limit");
     
-    $mirmo = array();
-    while($row=mysqli_fetch_assoc($All_Users))
-    {
-        $mirmo[] = $row;
-    }
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($mirmo);   
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $meow = $row["contact"]. "|";
+            $cat = preg_replace("/\|$/", "", $meow);
+        }
 
-    $all_data=mysqli_query($con,"select count(*) from tblusers");
-    $user_count = mysqli_fetch_row($all_data);   // say total count 9  
-    $total_records = $user_count[0];   //9
-    $total_pages = ceil($total_records / $limit);    // 9/3=  3
+        echo $cat;
+    } else {
+        echo "0 results";
+    }
+    
+    //$mirmo = array();
+    //while($row=mysqli_fetch_assoc($All_Users))
+    //{
+    //   $mirmo[] = $row;
+    //}
+    //header('Content-Type: txt; charset=utf-8');
+    //echo json_encode($mirmo);   
 
         //close the db connections
         mysqli_close($con);
