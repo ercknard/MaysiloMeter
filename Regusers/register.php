8d6582d231098346754b43d0ccf48f09a0fb9fa4
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-10 07:52:05 +0800

    Update register.php

--- Regusers/register.php
+++ Regusers/register.php
@@ -1,6 +1,13 @@
-<?php 
+<?php
+// Initialize the session
 session_start();
-?> 
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["userloggedin"]) || $_SESSION["userloggedin"] !== true){
+    header("location: Login/login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
