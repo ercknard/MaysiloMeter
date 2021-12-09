80fb8bc07a5a9e03a25717f8041b5b3b0527b7f5
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-09 10:03:41 +0800

    Update Diagrams.php

--- Admin/AdminDashboard/Diagrams.php
+++ Admin/AdminDashboard/Diagrams.php
@@ -115,11 +115,13 @@ h5 {
                         <h2 class="pull-left">Diagrams      </h2>
                         <a href="../welcome.php" class="btn btn-primary pull-right ml-2">Back</a>
                     </div>
+                    <p align="center">
                     <h5> Water Guage :</h5>
-                    <iframe width="430px" height="400px" frameborder="1px" src=" https://industrial.ubidots.com/app/dashboards/public/widget/p3U4z_iXN9hVQW8cNvudfDosppSxQmH3d40GjKWcAuY?embed=true"></iframe>
+                    <iframe width="400px" height="400px" frameborder="1px" src=" https://industrial.ubidots.com/app/dashboards/public/widget/p3U4z_iXN9hVQW8cNvudfDosppSxQmH3d40GjKWcAuY?embed=true"></iframe>
                     <h5> Meter Guage :</h5>
-                    <iframe width="430px" height="400px" frameborder="1px" src=" https://industrial.ubidots.com/app/dashboards/public/widget/ZfaeOHgpncrcSSDQ78hQyjIe-Q6TSKSh_O4BGogxKbk?embed=true"></iframe>
+                    <iframe width="400px" height="400px" frameborder="1px" src=" https://industrial.ubidots.com/app/dashboards/public/widget/ZfaeOHgpncrcSSDQ78hQyjIe-Q6TSKSh_O4BGogxKbk?embed=true"></iframe>
                     </div>
+                    </p>
             </div>        
         </div>
     </div>
