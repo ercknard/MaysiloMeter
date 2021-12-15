ce16540b0231b3afe2f8a5bd66a2fedeecaea11a
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-15 16:04:42 +0800

    12

--- Regusers/requesst.php
+++ Regusers/requesst.php
@@ -92,7 +92,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group">
                             <label>Username</label>
-                            <input type="text" name="username" placeholder="Enter registered username." class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                            <input type="text" name="username" placeholder="Enter your desired username." class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                             <span class="invalid-feedback"><?php echo $username_err;?></span>
                         </div>
                         <div class="form-group">
