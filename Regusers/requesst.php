c680cac2059af06823bc66c12cf52c9be9aa42b8
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-15 16:03:15 +0800

    12

--- Regusers/requesst.php
+++ Regusers/requesst.php
@@ -3,11 +3,6 @@
 // Initialize the session
 session_start();
  
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["userloggedin"]) || $_SESSION["userloggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
 ?>
 <?php
 // Include config file
