283113edebfc968074d1ce01e003229a8969be4f
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-28 20:42:00 +0800

    21

--- Admin/AdminDashboard/Tables.php
+++ Admin/AdminDashboard/Tables.php
@@ -111,7 +111,7 @@ body {
 
                     </div>
                     <div class="form-group">
-                    <a href="#" class="btn btn-primary" onclick="window.print();return false;">Print all</a>
+                    <a href="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true" class="btn btn-primary" onclick="window.print();return false;">Print all</a>
                     </div>
             </div>        
         </div>
