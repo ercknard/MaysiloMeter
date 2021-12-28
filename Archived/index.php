fd0cebf416c74f548729592f58ec05f5de5966b0
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-28 20:28:45 +0800

    12

--- Archived/index.php
+++ Archived/index.php
@@ -29,4 +29,6 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+<a href="#" onclick="window.print();return false;"><img src ="http://png.findicons.com/files/icons/61/dragon_soft/512/printer.png" width="20" height="20" /> Print all</a>|
+ 
 </html>
