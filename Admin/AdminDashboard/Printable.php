4c0e8972a113656379ac72c4dc255756b70e3ab9
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-30 05:22:00 +0800

    12

--- Admin/AdminDashboard/Printable.php
+++ Admin/AdminDashboard/Printable.php
@@ -3,6 +3,31 @@
     <title>Table</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<iframe width="40%" height="500px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
-    <p> Click the button below to generate PDF file </p>
-    <a href="Printable.php" class="btn btn-primary" onclick="window.print();return false;">Print</a>
+<style>
+        .wrapper{
+            width: 850px;
+            margin: auto;
+        }
+        table tr td:last-child{
+            width: 120px;
+        }
+    </style>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                    <hr color="lightblue" width="100%">
+                        <h2 class="pull-left">Table      </h2>
+                    </div>
+                    <iframe width="100%" height="500px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
+                    </div>
+                    <div class="form-group">
+                    <p> Click the button below to generate PDF file </p>
+                        <a href="Printable.php" class="btn btn-primary" onclick="window.print();return false;">Print</a>
+                    </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
