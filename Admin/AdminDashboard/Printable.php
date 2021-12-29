bbfd8310aeaddbe42db8eb2578cb868baefd2071
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-30 05:23:27 +0800

    12

--- Admin/AdminDashboard/Printable.php
+++ Admin/AdminDashboard/Printable.php
@@ -5,7 +5,7 @@
 </head>
 <style>
         .wrapper{
-            width: 850px;
+            width: 950px;
             margin: auto;
         }
         table tr td:last-child{
@@ -20,9 +20,10 @@
                     <hr color="lightblue" width="100%">
                         <h2 class="pull-left">Table      </h2>
                     </div>
-                    <iframe width="100%" height="500px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
+                    <iframe width="100%" height="600px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
                     </div>
                     <div class="form-group">
+                    <hr color="lightblue" width="100%">
                     <p> Click the button below to generate PDF file </p>
                         <a href="Printable.php" class="btn btn-primary" onclick="window.print();return false;">Print</a>
                     </div>
