e557f7040419ff4d32bd1c789d91addfdebc0422
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-30 05:02:32 +0800

    1212

--- Admin/AdminDashboard/Printable.php
+++ Admin/AdminDashboard/Printable.php
@@ -1,1 +1,2 @@
 <iframe width="100%" height="500px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
+    <a href="Printable.php" class="btn btn-primary" onclick="window.print();return false;">Print all</a>
