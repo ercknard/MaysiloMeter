59dbd3af66755e07b692ee37f4a593050ff6d706
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-30 04:02:47 +0800

    12

--- Admin/AdminDashboard/success.php
+++ Admin/AdminDashboard/success.php
@@ -14,9 +14,9 @@
 <img class="img-responsive center-block d-block mx-auto" src="/images/floodlogo.png" width="120px" height="120px">
 
 <hr color="lightblue" width="100%">
-                    <h2>Request Submission Result :</h2>
-                    <p>Your request is submitted successfully</p>
-                        <a href="register.php" class="btn btn-primary">GO Back</a>
+                    <h2>Post Submission Result :</h2>
+                    <p>Your Announcement is posted successfully</p>
+                        <a href="Announcements.php" class="btn btn-primary">GO Back</a>
                     </form>
                 </div>
             </div>        
