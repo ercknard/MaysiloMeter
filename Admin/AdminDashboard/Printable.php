a961991c9db0d28864af87c26998e980aa98434a
ercknard <79645928+ercknard@users.noreply.github.com>
2021-12-30 05:17:13 +0800

    12

--- Admin/AdminDashboard/Printable.php
+++ Admin/AdminDashboard/Printable.php
@@ -3,6 +3,6 @@
     <title>Table</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<iframe width="50%" height="500px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
+<iframe width="40%" height="500px" frameborder="0" src="https://industrial.ubidots.com/app/dashboards/public/widget/EIGS2UEfLeWZQZUCri6sSqHRx0wAdnReN1p7SCnJH5A?embed=true"></iframe>
     <p> Click the button below to generate PDF file </p>
-    <a href="Printable.php" class="btn btn-primary" onclick="window.print();return false;">Print all</a>
+    <a href="Printable.php" class="btn btn-primary" onclick="window.print();return false;">Print</a>
