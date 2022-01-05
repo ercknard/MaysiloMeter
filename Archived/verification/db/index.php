91a19756dacca254823960a9b68fb190019721b1
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-01-05 19:34:24 +0800

    12

--- Archived/verification/db/index.php
+++ Archived/verification/db/index.php
@@ -6,16 +6,4 @@ $dbname = "heroku_5142987c57081aa";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-if($conn->query($sql) === TRUE) {
-  echo "Table successfully updated";
-} else {
-  echo "Error updating table: " . $conn->error;
-}
 ?>
