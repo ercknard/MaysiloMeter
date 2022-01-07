98e0ba37bb4a157c869cb04160a6f156efea5eb8
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-01-07 16:17:19 +0800

    12

--- Receiver/index.php
+++ Receiver/index.php
@@ -4,7 +4,7 @@
 //open connection to mysql db
 $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($con));
 
-$limit = 10;
+$limit = 1;
 /*$result=mysqli_query($con,"SELECT COUNT(*) as total from tblusers");
 $user_count = mysqli_fetch_row($result);   // say total count 9  
 $total_records = $user_count[0];   //9
@@ -12,13 +12,13 @@ $total_pages = ceil($total_records / $limit);*/
 
   //set  Number of entries to show in a page.
     // Look for a GET variable page if not found default is 1.        
-    if (isset($_GET["page"])) {    
-    $page  = $_GET["page"];    
+    if (isset($_GET["record"])) {    
+    $record  = $_GET["record"];    
     }    
-    else { $page=1;    
+    else { $record=1;    
     } 
     //determine the sql LIMIT starting number for the results on the displaying page  
-    $page_index = ($page-1) * $limit;      // 0
+    $page_index = ($record-1) * $limit;      // 0
 
     $result=mysqli_query($con,"select contact from tblusers limit $page_index, $limit");
     $cactus = "";
