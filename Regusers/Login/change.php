40b34413fe7a66f99f2525f6e59826ef453f8e07
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-01-07 08:31:37 +0800

    12

--- Regusers/Login/change.php
+++ Regusers/Login/change.php
@@ -51,9 +51,7 @@ require_once "config.php";
                 <a class="btn btn-danger ml-2" href="logout.php">Sign out</a>
             </div>
         </form>
-        <form action="controllers/checkexists2.php" method="post">
-        <input type="submit" class="btn btn-primary" value="Unregister User">
-</form>
+        <input type="submit" action="controllers/checkexists2.php" method="post" class="btn btn-primary" value="Unregister User">
     </div>    
 </body>
 </html>
